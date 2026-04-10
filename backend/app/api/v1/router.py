0980652f39f4d0cfc6445facc9eb8eee57b17687
Renan Alves <renannalves334@gmail.com>
2026-04-10 13:07:26 -0300

    Colocando rota de setup

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from .endpoints import clinica, auth, assinatura, analise
+from .endpoints import clinica, auth, assinatura, analise, criar_user
 api_router = APIRouter()
 
 api_router.include_router(clinica.router, prefix="/clinica", tags=["Gestão de Clínicas"])
 api_router.include_router(auth.router, prefix="/auth", tags=["Autenticacão"])
 api_router.include_router(assinatura.router, prefix="/assinatura", tags=["Assinaturas"])
 api_router.include_router(analise.router, prefix="/analise", tags=["Analise de Dados"])
+api_router.include_router(criar_user.router, prefix="/setup")
